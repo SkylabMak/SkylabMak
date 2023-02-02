3b3b2e96fb1d85416aa3b37ca1fe0fe5ccbbd55c
SkylabMak <skylab135@gmail.com>
2023-02-02 21:21:02 +0700

    debug main page 04

--- index.js
+++ index.js
@@ -21,12 +21,8 @@ app.use(express.static(__dirname +"public/meteor-shot"));
 app.use(express.static(__dirname +"public/42rush"));
 
 app.get('', function (req, res) {
-    //res.send("this is my website test")
-    res.sendFile(path.join(__dirname, '/public/main/Html/index.html'));
-});
-app.get('/', function (req, res) {
-    //res.send("this is my website test")
-    res.sendFile(path.join(__dirname, '/public/main/Html/index.html'));
+    res.send("this is my website test")
+    //res.sendFile(path.join(__dirname, '/public/main/Html/index.html'));
 });
 app.get('/friend', function (req, res) {
     res.sendFile(path.join(__dirname, '/public/friend/friendMain.html'));
